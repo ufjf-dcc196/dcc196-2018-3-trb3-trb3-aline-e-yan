c204e5595cfbcb44a2cd05e3443685a5f51ddc54
victorreis <v.crisostomo.reis@gmail.com>
2018-12-06 01:06:12 -0200

    Criado o layout do visualizar estudante.

---
+++ app/src/main/java/trab3/dcc196/ufjf/br/trabalho3/activities/VisualizarEstudanteActivity.java
@@ -0,0 +1,51 @@
+package trab3.dcc196.ufjf.br.trabalho3.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import trab3.dcc196.ufjf.br.trabalho3.R;
+
+public class VisualizarEstudanteActivity extends AppCompatActivity {
+
+    private TextView txtNomeCompleto;
+    private TextView txtCPF;
+    private TextView txtNomeEscola;
+    private TextView txtEndereco;
+    private TextView txtCidade;
+    private TextView txtEstado;
+
+    private Button btnEditarEstudante;
+    private Button btnVisualizarLocalizacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_estudante);
+
+        txtNomeCompleto = (TextView) findViewById(R.id.txt_nome_completo);
+        txtCPF = (TextView) findViewById(R.id.txt_cpf);
+        txtNomeEscola = (TextView) findViewById(R.id.txt_nome_escola);
+        txtEndereco = (TextView) findViewById(R.id.txt_endereco);
+        txtCidade = (TextView) findViewById(R.id.txt_cidade);
+        txtEstado = (TextView) findViewById(R.id.txt_estado);
+
+        btnEditarEstudante = (Button) findViewById(R.id.btn_editar_estudante);
+        btnEditarEstudante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnVisualizarLocalizacao = (Button) findViewById(R.id.btn_visualizar_localizacao);
+        btnVisualizarLocalizacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
